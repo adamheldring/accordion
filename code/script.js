4eef46cc81a3b937f9232d2e6924db5269cb9a1e
A-Game <javascript@adamheldring.com>
2018-09-17 23:49:58 +0200

    Added main parts

--- code/script.js
+++ code/script.js
@@ -3,5 +3,10 @@ function toggle() {
   this.classList.toggle("active")
 }
 
-// Selects and HTML element, and calls a function which will be executed when the element is clicked.
-document.getElementById("section1").onclick = toggle
+// Selects an HTML element, and calls a function which will be executed when the element is clicked.
+document.getElementById("section1").onclick = toggle;
+document.getElementById("section2").onclick = toggle;
+document.getElementById("section3").onclick = toggle;
+document.getElementById("section4").onclick = toggle;
+
+console.log('Hello');
