f35a8ffa23fac5583f55181e3012c6fb5927a9ab
A-Game <javascript@adamheldring.com>
2018-09-19 14:34:30 +0200

    Clean-up

--- code/script.js
+++ code/script.js
@@ -1,12 +1,24 @@
 // A function that adds and remove the class "active" on the section you click on.
-function toggle() {
-  this.classList.toggle("active")
+//..and now also uses jQuery to slide connected descriptions up and down
+
+var descVisible = [false, false, false, false, false];
+
+function toggle(e) {
+  this.classList.toggle("active");
+  for (let i = 1; i <= 4; i++) {
+    if (this.id == "section" + i) {
+      if (descVisible[i-1] == false) {
+        $('#description' + i).slideDown(1000);
+        descVisible[i-1] = true;
+      } else {
+        $('#description' + i).slideUp(1000);
+        descVisible[i-1] = false;
+      }
+    }
+  }
 }
 
-// Selects an HTML element, and calls a function which will be executed when the element is clicked.
 document.getElementById("section1").onclick = toggle;
 document.getElementById("section2").onclick = toggle;
 document.getElementById("section3").onclick = toggle;
 document.getElementById("section4").onclick = toggle;
-
-console.log('Hello');
